9f96e861f3f6749b12ca64d6b593c2457d49d808
Ricardo Borges de Carvalho <121138227+RicardoCarvalhoGit@users.noreply.github.com>
2023-05-30 09:33:40 -0300

    Update main.py
    refinamento do código

--- main.py
+++ main.py
@@ -31,8 +31,10 @@ while continua == 's':
 
   if contador == 1:
     print (contador, "pessoa já respondeu")
+    print ("********************")
   else:
     print (contador, "pessoas já responderam")
+    print ("********************")
 
 
   if idade > maiorIdade:
@@ -53,27 +55,43 @@ while continua == 's':
   maiorPorcento = maior25 / contador
 
 
-print ("Nome")
+print ("Lista de dados")
 print ("********************")
 for i in range (len(nomes)):
   print(nomes [i]," tem ", idades[i])
-print ("Média = ", media)
+print ("********************")
+print ("Média das idades = ", media)
+print ("********************")
 print ("Quantidade de alunos = ", contador)
+print ("********************")
 print ("Maior idade = ", maiorIdade)
 print ("Menor idade = ", menorIdade)
+print ("********************")
 print ("Pessoas com menos de 18 anos = ", menor18)
 print ("Pessoas entre 18 e 25 anos = ", entre18e25)
 print ("Pessoas com mais de 25 anos = ", maior25)
+print ("********************")
 print ("Percentual de pessoas com menos de 18 = ", menorPorcento * 100, "%")
 print ("Percentual de pessoas entre 18 e 25 = ", entrePorcento * 100, "%")
 print ("Percentual de pessoas com mais de 25 = ", maiorPorcento * 100, "%")
 
 
-nomespesq = input("Deseja saber a idade do(a):")
-for i in range(len(nomes)):
+continua = 'n'
+
+
+while continua == 'n':
+  
+  print ("********************")
+  nomespesq = input("Deseja saber a idade do(a):\n")
+  for i in range(len(nomes)):
+    if nomespesq == nomes[i]:
+      break
   if nomespesq == nomes[i]:
-    break
-if nomespesq == nomes[i]:
+    continua = 's'
     print("A idade de ", nomespesq, " é ",idades[i])
-else:
-    print ("Pessoa não incontrada!!")
+  else:
+    continua = 'n'
+    print ("********************")
+    print ('Erro! Pessoa não encontrada.')
+    input ('Aperte enter para pesquisar novamente.')
+    os.system('cls')
